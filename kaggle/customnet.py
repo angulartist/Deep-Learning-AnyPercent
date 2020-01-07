85b6e17b6c913f7d4892d64da1e4020b45a9c21b
techlead <belabesmohammed@gmail.com>
2020-01-07 21:17:48 +0100

    kaggle mnist 0.999 acc

---
+++ kaggle/customnet.py
@@ -0,0 +1,44 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
+
+class CustomNet(object):
+    @staticmethod
+    def build(width, height, num_classes, depth=3):
+        model = Sequential()
+        input_shape = (height, width, depth)
+        chan_dim = -1
+        
+        # (CONV => ReLU => BN) * 2 => POOL => DO
+        model.add(Conv2D(32, (3, 3), padding='same', input_shape=input_shape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(Conv2D(32, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        
+         # (CONV => ReLU => BN) * 2 => POOL => DO
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        
+        # FC => ReLU
+        model.add(Flatten())
+        model.add(Dense(256))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        # Softmax
+        model.add(Dense(num_classes))
+        model.add(Activation('softmax'))
+        
+        print(model.summary())
+        
+        return model
