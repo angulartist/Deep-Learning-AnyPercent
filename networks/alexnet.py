73275b869cbdfdb7c81fd3468688fe269ed79859
techlead <belabesmohammed@gmail.com>
2020-01-09 20:54:20 +0100

    refact

--- networks/alexnet.py
+++ networks/alexnet.py
@@ -1,21 +1,22 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
+from keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
+from keras.models import Sequential
+
 
 class AlexNet(object):
     @staticmethod
-    def build(width, height, num_classes, depth=3): # Depth is 3 cuz' RGB color-space
+    def build(width, height, num_classes, depth=3):
         model = Sequential()
         input_shape = (height, width, depth)
-        chan_dim = -1 # Index of the channel dimension (in order to apply BN)
-        
+        chan_dim = -1
+
         # First layers set
         # 96 filters
         model.add(Conv2D(96, (11, 11), strides=(4, 4), padding='same', input_shape=input_shape))
         model.add(Activation('relu'))
         model.add(BatchNormalization(axis=chan_dim))
-        model.add(MaxPooling2D(pool_size=(3, 3))) # Implicit stride of 2x2
+        model.add(MaxPooling2D(pool_size=(3, 3)))
         model.add(Dropout(0.25))
-        
+
         # Second layers set
         # 256 filters
         model.add(Conv2D(256, (5, 5), padding='same'))
@@ -23,9 +24,9 @@ class AlexNet(object):
         model.add(BatchNormalization(axis=chan_dim))
         model.add(MaxPooling2D(pool_size=(3, 3)))
         model.add(Dropout(0.25))
-        
+
         # Third layers set
-        # 384 + 384 + 256 filters
+        # 384*2 + 256 filters
         model.add(Conv2D(384, (3, 3), padding='same'))
         model.add(Activation('relu'))
         model.add(BatchNormalization(axis=chan_dim))
@@ -37,24 +38,24 @@ class AlexNet(object):
         model.add(BatchNormalization(axis=chan_dim))
         model.add(MaxPooling2D(pool_size=(3, 3)))
         model.add(Dropout(0.25))
-        
+
         # Fourth layers set
         model.add(Flatten())
         model.add(Dense(4096))
         model.add(Activation('relu'))
         model.add(BatchNormalization())
         model.add(Dropout(0.5))
-        
+
         # Fifth layers set
         model.add(Dense(4096))
         model.add(Activation('relu'))
         model.add(BatchNormalization())
         model.add(Dropout(0.5))
-        
+
         # Softmax Classifier
-        model.add(Dense(num_classes)) # Determined by the number of classes
+        model.add(Dense(num_classes))
         model.add(Activation('softmax'))
-        
+
         print(model.summary())
-        
+
         return model
--- networks/smallgooglenet.py
+++ networks/smallgooglenet.py
@@ -1,6 +1,7 @@
-from tensorflow.keras.layers import BatchNormalization, Conv2D, AveragePooling2D, MaxPooling2D, Activation, Flatten, Dropout, Dense, Input, concatenate
-from tensorflow.keras.models import Model
-from tensorflow.keras import backend as K
+from keras.layers import BatchNormalization, Conv2D, AveragePooling2D, MaxPooling2D, Activation, Flatten, Dropout, \
+    Dense, Input, concatenate
+from keras.models import Model
+
 
 class SmallGoogleNet(object):
     @staticmethod
@@ -8,52 +9,51 @@ class SmallGoogleNet(object):
         x = Conv2D(K, (kX, kY), strides=strides, padding=padding)(x)
         x = BatchNormalization(axis=chan_dim)(x)
         x = Activation('relu')(x)
-        
+
         return x
-    
+
     @staticmethod
     def inception_module(x, num_K1x1, num_K3x3, chan_dim):
         conv_1x1 = SmallGoogleNet.conv_module(x, num_K1x1, 1, 1, (1, 1), chan_dim)
         conv_3x3 = SmallGoogleNet.conv_module(x, num_K3x3, 3, 3, (1, 1), chan_dim)
-            
+
         return concatenate([conv_1x1, conv_3x3], axis=chan_dim)
-        
+
     @staticmethod
     def downsample_module(x, K, chan_dim):
         conv_3x3 = SmallGoogleNet.conv_module(x, K, 3, 3, (2, 2),
                                               chan_dim, padding='valid')
         pool = MaxPooling2D((3, 3), strides=(2, 2))(x)
-            
+
         return concatenate([conv_3x3, pool], axis=chan_dim)
-        
+
     @staticmethod
     def build(width, height, num_classes, depth=3):
         input_shape = (height, width, depth)
-        chan_dim = -1 # Index of the channel dimension (in order to apply BN)
-        
+        chan_dim = -1
+
         inputs = Input(shape=input_shape)
         x = SmallGoogleNet.conv_module(inputs, 96, 3, 3, (1, 1), chan_dim)
         x = SmallGoogleNet.inception_module(x, 32, 32, chan_dim)
         x = SmallGoogleNet.inception_module(x, 32, 48, chan_dim)
         x = SmallGoogleNet.downsample_module(x, 80, chan_dim)
-        
+
         x = SmallGoogleNet.inception_module(x, 112, 48, chan_dim)
         x = SmallGoogleNet.inception_module(x, 96, 64, chan_dim)
         x = SmallGoogleNet.inception_module(x, 80, 80, chan_dim)
         x = SmallGoogleNet.inception_module(x, 48, 96, chan_dim)
         x = SmallGoogleNet.downsample_module(x, 96, chan_dim)
-        
+
         x = SmallGoogleNet.inception_module(x, 176, 160, chan_dim)
         x = SmallGoogleNet.inception_module(x, 176, 160, chan_dim)
         x = AveragePooling2D((7, 7))(x)
         x = Dropout(0.5)(x)
-        
+
         x = Flatten()(x)
         x = Dense(num_classes)(x)
-        
+
         model = Model(inputs, x, name='googlenet')
-        
+
         print(model.summary())
-        
+
         return model
-        
