448e8157f195ef1b0034df1856abbfed3a127c05
techlead <belabesmohammed@gmail.com>
2020-01-09 22:51:01 +0100

    added explanation 1/2

--- toolkit/viz.py
+++ toolkit/viz.py
@@ -2,6 +2,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def plot_images_grid(images, rows=5, cols=4):
+    fig_size = [6, 8]
+
+    fig, ax = plt.subplots(
+        nrows=rows,
+        ncols=cols,
+        figsize=fig_size
+    )
+
+    for index, axi in enumerate(ax.flat):
+        axi.imshow(images[index])
+        axi.set_title(f'Image #{index}')
+
+    plt.tight_layout(True)
+    plt.show()
+
+
 def plot_loss(num_epochs, h):
     plt.plot(np.arange(0, num_epochs), h.history['loss'], label='train_loss')
     plt.plot(np.arange(0, num_epochs), h.history['val_loss'], label='val_loss')
